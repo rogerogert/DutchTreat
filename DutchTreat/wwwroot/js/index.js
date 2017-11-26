eebf378468e2ac087516fe81176fddf3b5c28631
rogerogert <rogerio.teixeira@inspira.com.br>
2017-11-26 16:52:14 -0200

    Javascript Events

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,2 +1,10 @@
 ﻿var theForm = document.getElementById("theForm");
 theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Comprando um item");
+});
+
+var productInfo = document.getElementByClassName("product-props");
+var listItems = productInfo.item[0].children;
