82a3d43f8b7aecb3be9be05df4649b8dcc88492b
rogerogert <rogerio.teixeira@inspira.com.br>
2017-11-30 20:29:58 -0200

    Model Binding

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,7 @@ namespace DutchTreat.Controllers
 			return View();
 		}
 		[HttpPost("contact")]
-		public IActionResult Contact(object model)
+		public IActionResult Contact(ContactViewModel model)
 		{
 			return View();
 		}
