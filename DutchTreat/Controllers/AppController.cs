e114112b4f2d2cc62cd98a76ef04383b83d234af
Rogerio Teixeira <rogerioteixeira91@gmail.com>
2018-03-26 16:41:21 -0300

    [EFCoreModule] Using DbContext

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using DutchTreat.Services;
+﻿using DutchTreat.Data;
+using DutchTreat.Services;
 using DutchTreat.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,40 +10,49 @@ using System.Threading.Tasks;
 
 namespace DutchTreat.Controllers
 {
-    public class AppController: Controller
+    public class AppController : Controller
     {
-		private readonly IMailService _mailService;
+        private readonly IMailService _mailService;
+        private readonly DutchContext _context;
 
-		public AppController(IMailService mailService)
-		{
-			_mailService = mailService;
-		}
+        public AppController(IMailService mailService DutchContext context)
+        {
+            _mailService = mailService;
+            _context = context;
+        }
         public IActionResult Index()
-		{
-			return View();
-		}
+        {
+            return View();
+        }
 
-		[HttpGet("contact")]
-		public IActionResult Contact()
-		{
-			return View();
-		}
-		[HttpPost("contact")]
-		public IActionResult Contact(ContactViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				_mailService.SendMessage("rogerio@inspira.com.br", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
-				ViewBag.UserMessage = "Mail Sent";
-				ModelState.Clear();
-			}
-			
-			return View();
-		}
-		public IActionResult About()
-		{
-			ViewBag.Title = "About";
-			return View();
-		}
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("rogerio@inspira.com.br", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            var results = from p in _context.Products
+                          orderby p.Category
+                          select p;
+            return View(results.ToList());
+        }
     }
 }
